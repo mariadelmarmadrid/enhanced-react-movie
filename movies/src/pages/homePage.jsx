4be5dd785a793041e7346ed2063bc99dfa430435
Mdelmar05 <mmarmadridel@uma.es>
2025-10-07 11:46:59 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -24,6 +24,12 @@ const HomePage = (props) => {
     else setGenreFilter(value);
   };
 
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
 
 
   useEffect(() => {
@@ -54,7 +60,7 @@ const HomePage = (props) => {
           />
 
         </Grid>
-        <MovieList movies={displayedMovies} />
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );
