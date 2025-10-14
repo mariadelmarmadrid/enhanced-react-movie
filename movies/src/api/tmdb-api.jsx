01d1f698f8b750fc9283626b34f7175176353086
Mdelmar05 <mmarmadridel@uma.es>
2025-10-14 13:57:52 +0100

    Added Upcoming movies page.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -92,3 +92,18 @@ export const getMovieReviews = ({ queryKey }) => {
 };
 
 
+export const getUpcomingMovies = () => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    ).then((response) => {
+        if (!response.ok) {
+            return response.json().then((error) => {
+                throw new Error(error.status_message || "Something went wrong");
+            });
+        }
+        return response.json();
+    })
+        .catch((error) => {
+            throw error
+        });
+};
